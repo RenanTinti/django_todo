a47da7d56b8287dacd83849c818e03ad95f96741
Renan Tinti <renantinti@gmail.com>
2022-09-21 16:46:50 -0300

    configuring settings and requirements

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nui2dg)a5$&(lbylynm^t6jet)c25+mcj%1q&q3tgm-v5*w*86
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
