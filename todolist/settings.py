a1259a35ff8a4486ac4bddae76344af4b15a3447
Renan Tinti <renantinti@gmail.com>
2022-09-22 12:13:01 -0300

    added csrf trusted origins

--- todolist/settings.py
+++ todolist/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-8c32.up.railway.app/'
+]
